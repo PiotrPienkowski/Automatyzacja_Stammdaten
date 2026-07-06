56bf2563a1488aa79e66a01c30c8b90f6a34c011
Piotr Pienkowski <pienkowski.piotr1984@gmail.com>
2026-07-06 20:50:08 +0200

    06072026 - dodano funkcje C06 C34 i C33

--- GTS.py
+++ GTS.py
@@ -9,7 +9,7 @@ def C08(CN,BTM):
 
     ws.Range('E5').Value = CN
     ws.Range('E23').Value = "C08"
-    ws.Range('E59').Value = 'yes'
+    ws.Range('E59').Value = 'Yes'
     ws.Range('E60').Value = 'C08 - DEA Licence/Narcotic'
     ws.Range('E61').Value = BTM
     ws.Range('E62').Value = '30.12.9999'
@@ -44,7 +44,80 @@ def C08(CN,BTM):
     wb1.Close(SaveChanges=False)
     excel.Quit()
 
-C08('50021166','252452')
+
+def C06(CN):
+    excel = win32.Dispatch('Excel.Application')
+    excel.Visible = False
+    wb = excel.Workbooks.Open(r'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\CMD_template4.1.4.xlsm')
+    ws = wb.Worksheets('Sheet1')
+
+    ws.Range('E5').Value = CN
+    ws.Range('E23').Value = "C06"
+    ws.Range('E59').Value = 'Yes'
+    ws.Range('E60').Value = 'C06 - Veterinary'
+    ws.Range('E61').Value = 'NA'
+    ws.Range('E62').Value = '30.12.9999'
+    ws.Range('E63').Value = 'L01, L02, L03, L04, NONE'
+    ws.Range('E64').Value = 'NA'
+    ws.Range('E64').Value = '9,999,999'
+
+    path = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{CN} create C06 licence.xlsm'
+    wb.SaveAs(path)
+    wb.Close(SaveChanges=False)
+    excel.Quit()
+
+
+
+def C34(CN):
+    excel = win32.Dispatch('Excel.Application')
+    excel.Visible = False
+    wb = excel.Workbooks.Open(r'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\CMD_template4.1.4.xlsm')
+    ws = wb.Worksheets('Sheet1')
+
+    ws.Range('E5').Value = CN
+    ws.Range('E23').Value = "C34"
+    ws.Range('E59').Value = 'Yes'
+    ws.Range('E60').Value = 'C34 - Registration for Complementary Feed for Farm Animals'
+    ws.Range('E61').Value = 'NA'
+    ws.Range('E62').Value = '30.12.9999'
+    ws.Range('E63').Value = 'L10'
+    ws.Range('E64').Value = 'NA'
+    ws.Range('E65').Value = '9,999,999'
+
+    path = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{CN} create C34 licence.xlsm'
+    wb.SaveAs(path)
+    wb.Close(SaveChanges=False)
+    excel.Quit()
+
+def C33(CN):
+    excel = win32.Dispatch('Excel.Application')
+    excel.Visible = False
+    wb = excel.Workbooks.Open(r'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\CMD_template4.1.4.xlsm')
+    ws = wb.Worksheets('Sheet1')
+
+    ws.Range('E5').Value = CN
+    ws.Range('E23').Value = "C33"
+    ws.Range('E59').Value = 'Yes'
+    ws.Range('E60').Value = 'C33 - Vet Samples'
+    ws.Range('E61').Value = 'NA'
+    ws.Range('E62').Value = '30.12.9999'
+    ws.Range('E63').Value = 'NA'
+    ws.Range('E64').Value = 'CA5536030GQZ1, CA5537030GQZ1, CA5538030GQZ1, CA5539030GQZ1'
+    ws.Range('E65').Value = '2'
+
+    path = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{CN} create C33 licence.xlsm'
+    wb.SaveAs(path)
+    wb.Close(SaveChanges=False)
+    excel.Quit()
+
+
+C33('53633')
+
+
+
+
+
+
 
 
 
