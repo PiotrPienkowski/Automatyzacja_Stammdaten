05fca3214fdc26bfb4a9256aa6726a936c02711c
Piotr Pienkowski <pienkowski.piotr1984@gmail.com>
2026-07-09 21:34:25 +0200

    06072026 - dokonczone funkcje C08,C33.C34,c06 z vba z wyslaniem maili C08 do pharmloga

--- GTS.py
+++ GTS.py
@@ -1,11 +1,13 @@
 import win32com.client as win32
 import pandas as pd
+import os
 
 def C08(CN,BTM):
     excel = win32.Dispatch('Excel.Application')
     excel.Visible = False
     wb = excel.Workbooks.Open(r'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\CMD_template4.1.4.xlsm')
     ws = wb.Worksheets('Sheet1')
+
 
 # runowanie makra vba
 
@@ -46,6 +48,43 @@ def C08(CN,BTM):
     wb1.Close(SaveChanges=False)
     excel.Quit()
 
+    outlook = win32.Dispatch('Outlook.Application')
+    new_mail = outlook.CreateItem(0)
+    new_mail.To = 'tl@pharmlog.de;btm@pharmlog.de'
+    new_mail.CC = 'stammdaten@elancoah.com'
+    new_mail.Subject = (rf'{CN} BTM')
+    new_mail.Body = (f'Guten Tag,\n '
+                     f'Anbei BTM \n\n\n\n '
+                     f"Mit freundlichen Grüßen\n\n"
+                     f"Piotr Pieńkowski\n"
+                     f"Master Data Manager DACH\n\n"
+                     f"Elanco Deutschland GmbH\n"
+                     f"Rolf-Schwarz-Schütte-Platz 2 / CC A01 1.OG\n"
+                     f"D-40789 Monheim am Rhein\n"
+                     f"piotr.pienkowski@elancoah.com\n"
+                     f"www.elanco.de\n\n"
+                     f"AG Bad Homburg HRB 13307 | Geschäftsführung:\n"
+                     f"Dr. Inga Drosse und Cindy Wichmann\n"
+                     f"Sitz der Gesellschaft: Bad Homburg\n\n"
+                     f"CONFIDENTIALITY NOTICE: This email message (including all attachments) "
+                     f"is for the sole use of the intended recipient(s) and may contain confidential "
+                     f"and privileged information. Any unauthorized review, use, disclosure, "
+                     f"copying or distribution is strictly prohibited. If you are not the intended "
+                     f"recipient, please contact the sender by reply email and destroy all copies "
+                     f"of the original message.\n\n"
+                     f"PRIVACY NOTICE: Your privacy is important to us. To find out more about "
+                     f"the information that Elanco may collect, how we use it, how we protect it, "
+                     f"and your rights and choices with respect to your Personal Data, go to "
+                     f"privacy.elanco.com"
+                     )
+    path = r'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze'
+    new_mail.Attachments.Add(rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\pharmlog {CN}.xlsx')
+
+    for file in os.listdir(path):
+        if file.lower().endswith('.pdf'):
+            new_mail.Attachments.Add(os.path.join(path, file))
+
+    new_mail.Display()
 
 def C06(CN):
     excel = win32.Dispatch('Excel.Application')
@@ -116,4 +155,4 @@ def C33(CN):
 
 #######################
 
-C34('50017859')
+C08('50019163','123')
