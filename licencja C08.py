08863d6999aa33948b683827bac4569aae04b3d1
Piotr Pienkowski <pienkowski.piotr1984@gmail.com>
2026-07-12 17:21:21 +0200

    12.07.2026 - podzielono na C08 i pozostale licencje- pliki dizalaja

---
+++ licencja C08.py
@@ -0,0 +1,105 @@
+import win32com.client as win32
+import pandas as pd
+import os
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+
+
+def C08(CN, BTM):
+    excel = win32.DispatchEx('Excel.Application')
+    excel.Visible = False
+    wb = excel.Workbooks.Open(r'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\CMD_template4.1.4.xlsm')
+    ws = wb.Worksheets('Sheet1')
+    ws.Range('A12').Value = 'DE01'
+    ws.Range('B12').Value = 'Change'
+    ws.Range('C12').Value = 'Sold-to'
+    excel.Application.Run("CreatingHeader")
+
+    excel.CalculateUntilAsyncQueriesDone()
+    # runowanie makra vba
+
+    ws.Range('E5').Value = CN
+    ws.Range('E23').Value = "C08"
+    ws.Range('E59').Value = 'Yes'
+    ws.Range('E60').Value = 'C08 - DEA Licence/Narcotic'
+    ws.Range('E61').Value = BTM
+
+    new_file = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{CN}_create C08 licence.xlsm'
+    wb.SaveAs(new_file)
+    wb.Close(SaveChanges=False)
+
+    wb1 = excel.Workbooks.Open(r'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\BTM Template.xlsx')
+    ws1 = wb1.Worksheets(1)
+    wb1.RefreshAll()
+    excel.CalculateUntilAsyncQueriesDone()
+    data1 = ws1.UsedRange.Value
+    df = pd.DataFrame(data1)
+
+    # pierwszy wiersz -> nagłówki
+    df.columns = df.iloc[0]
+
+    # usuń pierwszy wiersz
+    df = df.iloc[1:].reset_index(drop=True)
+    df = df[df['Kundennummer'].astype(str).str.replace('.0', '', regex=False) == CN]
+    df['KLIENT'] = '342'
+    df['NR_BTM'] = BTM
+
+    new_file1 = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\pharmlog {CN}.xlsx'
+    df.to_excel(new_file1, index=False)
+
+    wb3 = load_workbook(new_file1)
+    ws3 = wb3.active
+
+    colour = PatternFill(fill_type='solid', fgColor='C0C0C0')
+
+    for cell in ws3[1]:
+        cell.fill = colour
+        ws3.column_dimensions[cell.column_letter].width = 20
+
+    for col in ['C', 'D', 'E', 'F']:
+        ws3.column_dimensions[col].width = 40
+
+    wb3.save(new_file1)
+    wb3.close()
+
+    wb1.Close(SaveChanges=False)
+
+    outlook = win32.Dispatch('Outlook.Application')
+    new_mail = outlook.CreateItem(0)
+    new_mail.To = 'tl@pharmlog.de;btm@pharmlog.de'
+    new_mail.CC = 'stammdaten@elancoah.com'
+    new_mail.Subject = (rf'{CN} BTM')
+    new_mail.Body = (f'Guten Tag,\n '
+                     f'Anbei BTM \n\n\n\n '
+                     f"Mit freundlichen Grüßen\n\n"
+                     f"Piotr Pieńkowski\n"
+                     f"Master Data Manager DACH\n\n"
+                     f"Elanco Deutschland GmbH\n"
+                     f"Rolf-Schwarz-Schütte-Platz 2 / CC A01 1.OG\n"
+                     f"D-40789 Monheim am Rhein\n"
+                     f"piotr.pienkowski@elancoah.com\n"
+                     f"www.elanco.de\n\n"
+                     f"AG Bad Homburg HRB 13307 | Geschäftsführung:\n"
+                     f"Dr. Inga Drosse und Cindy Wichmann\n"
+                     f"Sitz der Gesellschaft: Bad Homburg\n\n"
+                     f"CONFIDENTIALITY NOTICE: This email message (including all attachments) "
+                     f"is for the sole use of the intended recipient(s) and may contain confidential "
+                     f"and privileged information. Any unauthorized review, use, disclosure, "
+                     f"copying or distribution is strictly prohibited. If you are not the intended "
+                     f"recipient, please contact the sender by reply email and destroy all copies "
+                     f"of the original message.\n\n"
+                     f"PRIVACY NOTICE: Your privacy is important to us. To find out more about "
+                     f"the information that Elanco may collect, how we use it, how we protect it, "
+                     f"and your rights and choices with respect to your Personal Data, go to "
+                     f"privacy.elanco.com"
+                     )
+    path = r'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze'
+    new_mail.Attachments.Add(rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\pharmlog {CN}.xlsx')
+
+    for file in os.listdir(path):
+        if file.lower().endswith('.pdf'):
+            new_mail.Attachments.Add(os.path.join(path, file))
+
+    new_mail.Display()
+
+C08('50891051','4352')
---
+++ licencje C33 C34 i C06.py
@@ -0,0 +1,109 @@
+from openpyxl import load_workbook
+
+
+def C06(CN):
+    template = rf'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\Templatka do pythona GTS\CMD_template4.1.4.xlsm'
+    wb = load_workbook(template, keep_vba=True)
+    ws= wb['Sheet1']
+
+    ws['A12'] = 'DE01'
+    ws['B12'] = 'Change'
+    ws['C12']= 'Sold-to'
+
+    ws['E5'] = CN
+    ws['E23'] =  'C06'
+    ws['E59'] = 'Yes'
+    ws['E60'] = 'C06 - Veterinary'
+    ws['E61'] = 'NA'
+    ws['E62'] = '30.12.9999'
+    ws['E63'] = 'L01, L02, L03, L04, NONE'
+    ws['E64'] = 'NA'
+    ws['E65'] = '9,999,999'
+
+
+    sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{CN} create C06 licence.xlsm'
+
+    wb.save(sciezka)
+
+def C33(CN):
+    template = rf'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\Templatka do pythona GTS\CMD_template4.1.4.xlsm'
+    wb = load_workbook(template,keep_vba=True)
+    ws= wb['Sheet1']
+
+
+    ws['A12'] = 'DE01'
+    ws['B12'] = 'Change'
+    ws['C12']= 'Sold-to'
+
+
+
+    ws['E5'] = CN
+    ws['E23'] =  'C33'
+    ws['E59'] = 'Yes'
+    ws['E60'] = 'C33 - Vet Samples'
+    ws['E61'] = 'NA'
+    ws['E62'] =  '30.12.9999'
+    ws['E63'] = 'NA'
+    ws['E64'] = 'CA5536030GQZ1, CA5537030GQZ1, CA5538030GQZ1, CA5539030GQZ1'
+    ws['E65'] = '2'
+
+
+    sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{CN} create C33 licence.xlsm'
+
+    wb.save(sciezka)
+
+def C34(CN):
+    template = rf'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\CMD_template4.1.4.xlsm'
+    wb = load_workbook(template, keep_vba=True)
+    ws= wb['Sheet1']
+
+    ws['A12'] = 'DE01'
+    ws['B12'] = 'Change'
+    ws['C12']= 'Sold-to'
+
+
+
+    ws['E5'] = CN
+    ws['E23'] =  'C34'
+    ws['E59'] = 'Yes'
+    ws['E60'] = 'C33 - Vet Samples'
+    ws['E61'] = 'NA'
+    ws['E62'] =  '30.12.9999'
+    ws['E63'] = 'NA'
+    ws['E64'] = 'CA5536030GQZ1, CA5537030GQZ1, CA5538030GQZ1, CA5539030GQZ1'
+    ws['E65'] = '2'
+
+
+    sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{CN} create C33 licence.xlsm'
+
+    wb.save(sciezka)
+
+
+def C34(CN):
+    template = rf'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\Templatka do pythona GTS\CMD_template4.1.4.xlsm'
+    wb = load_workbook(template, keep_vba=True)
+    ws= wb['Sheet1']
+
+    ws['A12'] = 'DE01'
+    ws['B12'] = 'Change'
+    ws['C12']= 'Sold-to'
+
+
+
+    ws['E5'] = CN
+    ws['E23'] =  'C34'
+    ws['E59'] = 'Yes'
+    ws['E60'] = 'C34 - Registration for Complementary Feed for Farm Animals'
+    ws['E61'] = 'NA'
+    ws['E62'] =  '30.12.9999'
+    ws['E63'] = 'L10'
+    ws['E64'] = 'NA'
+    ws['E65'] = '9,999,999'
+
+
+    sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{CN} create C34 licence.xlsm'
+
+    wb.save(sciezka)
+    wb.close(sciezka)
+
+C34('452')
