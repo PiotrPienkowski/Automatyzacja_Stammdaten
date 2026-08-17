cb2d4d7f9852cc6054285060eff9bdfe545517c3
Piotr Pienkowski <pienkowski.piotr1984@gmail.com>
2026-08-17 10:53:57 +0200

    16.08.2026 - 'dodano playwrigghta do licencji C33 C34 i C97

--- licencje C33 C34 i C06.py
+++ licencje C33 C34 i C06.py
@@ -1,3 +1,4 @@
+from IPython.core import page
 from openpyxl import load_workbook
 import warnings
 import os
@@ -10,7 +11,7 @@ message="Data Validation extension is not supported and will be removed"
 )
 os.system("taskkill /F /IM excel.exe 1>nul 2>nul")
 
-snow = rf'https://thespot.elanco.com/esc?id=sc_cat_item&sys_id=9d661f191b03d1105ca7eca3604bcb3a&sysparm_category=a20cb8eedb7c60905513c3af299619d0'
+link_do_snow = rf'https://thespot.elanco.com/esc?id=sc_cat_item&sys_id=9d661f191b03d1105ca7eca3604bcb3a&sysparm_category=a20cb8eedb7c60905513c3af299619d0'
 path = rf'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\Templatka do pythona GTS\(sold-to  change  DE01)   CMD_template4.1.4.xlsm'
 
 class licencja:
@@ -24,12 +25,11 @@ class licencja:
         self.ws['C12'] = 'Sold-to'
         self.ws['E5'] = CN
 
-
     def snow_ticket(self):
         self.pw = sync_playwright().start()
         self.browser = self.pw.chromium.launch(channel= "chromium", headless= False)
         self.tab = self.browser.new_page()
-        self.tab.goto(snow)
+        self.tab.goto(link_do_snow)
         self.tab.locator("#s2id_sp_formfield_sales_organization a").click()
         self.tab.get_by_role("option", name="DE01").click()
         self.tab.locator("#s2id_sp_formfield_type_of_request a").click()
@@ -44,7 +44,13 @@ class licencja:
         self.tab.locator("#s2id_sp_formfield_multiple_requests a").click()
         self.tab.locator("#s2id_sp_formfield_account_group a").click()
         self.tab.get_by_role("option", name="Sold-to").click()
-        self.tab.get_by_role("textbox", name="Additional information").fill(r'Hello Team, please create licence (see attaced)')
+        self.page.get_by_role("textbox", name="Additional information").fill(
+            f'Hello Team, Pelase create C08 licence (See attached)')
+        with self.tab.expect_file_chooser() as fc:
+            self.tab.get_by_role("button", name = "Upload Attachment for CMD").click()
+        file_chooser = fc.value
+        file_chooser.set_files(self.sciezka)
+
 
 
     def C33(self):
@@ -56,7 +62,7 @@ class licencja:
         self.ws['E63'] = 'NA'
         self.ws['E64'] = 'CA5536030GQZ1, CA5537030GQZ1, CA5538030GQZ1, CA5539030GQZ1'
         self.ws['E65'] = '2'
-        self.sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{self.CN} create C33 licence.xlsm'
+        self.sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{self.CN} create {self.ws["E23"].value} licence.xlsm'
         self.wb.save(self.sciezka)
         self.snow_ticket()
         time.sleep(300)
@@ -70,7 +76,7 @@ class licencja:
         self.ws['E63'] = 'L01, L02, L03, L04, NONE'
         self.ws['E64'] = 'NA'
         self.ws['E65'] = '9,999,999'
-        self.sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{self.CN} ceate C06 licence.xlsm'
+        self.sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{self.CN} create {self.ws["E23"].value} licence.xlsm'
         self.wb.save(self.sciezka)
         self.snow_ticket()
         time.sleep(300)
@@ -84,10 +90,10 @@ class licencja:
         self.ws['E63'] = 'L10'
         self.ws['E64'] = 'NA'
         self.ws['E65'] = '9,999,999'
-        self.sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{self.CN} create C34 licence.xlsm'
+        self.sciezka = rf'C:\Users\02703821\OneDrive - Elanco\Desktop\robocze\{self.CN} create {self.ws["E23"].value} licence.xlsm'
         self.wb.save(self.sciezka)
         self.snow_ticket()
         time.sleep(300)
 
 
-licencja("112").C33()
+licencja("432").C06()
