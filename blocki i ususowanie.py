e3ca6fa309c4fcb67e00b42de87820493610b2ee
Piotr Pienkowski <pienkowski.piotr1984@gmail.com>
2026-09-02 11:55:04 +0200

    01.09.2026-'powerocono do stare ustrawienia i dodano veeva

--- blocki i ususowanie.py
+++ blocki i ususowanie.py
@@ -1,9 +1,10 @@
-import win32com.client
+
 import time
 import os
 from playwright.sync_api import sync_playwright
+from openpyxl import load_workbook
 
-template = r'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\Templatka do pythona GTS\(de close and open) CMD_template4.1.4.xlsm'
+template = r'C:\Users\02703821\Elanco\CH - Bestellung Monitoring\Templatka do pythona GTS\(de and ch sold-to block unblock).xlsm'
 new_file = r"C:\Users\02703821\OneDrive - Elanco\Desktop\robocze"
 snow = "https://thespot.elanco.com/esc?id=sc_cat_item&sys_id=9d661f191b03d1105ca7eca3604bcb3a&sysparm_category=a20cb8eedb7c60905513c3af299619d0"
 snow_ch = "https://thespot.elanco.com/esc?id=sc_cat_item&table=sc_cat_item&sys_id=666af21697260e907487fd7ef053afd3&recordUrl=com.glideapp.servicecatalog_cat_item_view.do%3Fv%3D1&sysparm_id=666af21697260e907487fd7ef053afd3"
@@ -15,12 +16,13 @@ class snow_ticket:
     def __init__(self):
 
         self.pw = sync_playwright().start()
-        self.browser = self.pw.chromium.launch(channel="chrome",headless=False)
+        self.browser = self.pw.chromium.launch(headless=False)
         self.page = self.browser.new_page()
 
     def snow_de(self, request_type = "Block/unblock"):
-        self.page.goto(snow)
-        self.page.wait_for_load_state("networkidle")
+        self.page.goto(snow, wait_until="domcontentloaded")
+        self.page.get_by_role("textbox", name="Enter your email, phone, or").fill("PIOTR.PIENKOWSKI@elancoah.com")
+        self.page.keyboard.press("Enter")
         self.page.locator("#s2id_sp_formfield_sales_organization a").click()
         self.page.get_by_role("option", name="DE01").click()
         self.page.locator("#s2id_sp_formfield_type_of_request a").click()
@@ -36,9 +38,10 @@ class snow_ticket:
         self.page.get_by_role("option", name="Sold-to").click()
         self.page.locator('#cmd_form_attached input[type="file"]').set_input_files(rf"{new_file}\{self.CN}.xlsm")
 
-
     def snow_ch(self,request_type = "Block/unblock"):
-        self.page.goto(snow_ch)
+        self.page.goto(snow_ch, wait_until="domcontentloaded")
+        self.page.get_by_role("textbox", name="Enter your email, phone, or").fill("PIOTR.PIENKOWSKI@elancoah.com")
+        self.page.keyboard.press("Enter")
         self.page.locator("#s2id_sp_formfield_type_of_request a").click()
         self.page.get_by_role("option", name= request_type ).click()
         self.page.locator("#s2id_sp_formfield_account_group a").click()
@@ -60,48 +63,41 @@ class de(snow_ticket):
     def __init__(self, CN):
         super().__init__()
         self.CN = CN
-        self.excel = win32com.client.gencache.EnsureDispatch("Excel.Application")
-        self.excel.Visible = False
-        self.excel.ScreenUpdating = False
-        self.excel.DisplayAlerts = False
-        self.wb = self.excel.Workbooks.Open(template)
-        self.ws = self.wb.Worksheets('Sheet1')
-        self.ws.Range('A12').Value = 'DE01'
-        self.ws.Range('B12').Value = 'Block/Unblock/Deletion Flag'
-        self.ws.Range('C12').Value = 'Sold-to'
-        self.excel.Application.Run("CreatingHeader")
-        self.excel.CalculateUntilAsyncQueriesDone()
-        self.ws.Range('E5').Value = CN
+        self.wb= load_workbook(template, keep_vba= True)
+        self.ws = self.wb.worksheets[0]
+        self.ws['A12'] = 'DE01'
+        self.ws['B12'] = 'Block/Unblock/Deletion Flag'
+        self.ws['C12'] = 'Sold-to'
+        self.ws['E5'] = CN
 
     def zapisywanie (self):
-        self.wb.SaveCopyAs(rf'{new_file}\{self.CN}.xlsm')
-        self.wb.Close()
-        self.excel.Quit()
+        self.wb.save(rf'{new_file}\{self.CN}.xlsm')
+        self.wb.close()
 
     def set_central_order_block(self):
-        self.ws.Range('E13').Value = 'Set'
-        self.ws.Range('E14').Value = '01 - Overall block'
+        self.ws['E13'] = 'Set'
+        self.ws['E14'] = '01 - Overall block'
         self.zapisywanie()
         self.snow_de()
         time.sleep(300)
 
     def remove_central_order_block(self):
-        self.ws.Range('E13').Value = 'Reset (Remove)'
-        self.ws.Range('E14').Value = '01 - Overall block'
+        self.ws['E13'] = 'Reset (Remove)'
+        self.ws['E14'] = '01 - Overall block'
         self.zapisywanie()
         self.snow_de()
         time.sleep(300)
 
     def remove_deletion_flag(self):
-        self.ws.Range('E12').Value = 'Reset (Remove)'
+        self.ws['E12'] = 'Reset (Remove)'
         self.zapisywanie()
         self.snow_de("Deletion Flag")
         time.sleep(300)
 
     def remove_central_order_and_deletion_flag(self):
-        self.ws.Range('E12').Value = 'Reset (Remove)'
-        self.ws.Range('E13').Value = 'Reset (Remove)'
-        self.ws.Range('E14').Value = '01 - Overall block'
+        self.ws['E12'] = 'Reset (Remove)'
+        self.ws['E13'] = 'Reset (Remove)'
+        self.ws['E14'] = '01 - Overall block'
         self.zapisywanie()
         self.snow_de("Deletion Flag")
         time.sleep(300)
@@ -110,42 +106,40 @@ class ch(de):
 
     def __init__(self, CN):
         super().__init__(CN)
-        self.ws.Range('A12').Value = 'CH01'
-        self.ws.Range('B12').Value = 'Block/Unblock/Deletion Flag'
-        self.ws.Range('C12').Value = 'Sold-to'
-        self.excel.Application.Run("CreatingHeader")
-        self.excel.CalculateUntilAsyncQueriesDone()
-        self.ws.Range('E5').Value = CN
+        self.ws['A12'] = 'CH01'
+        self.ws['B12'] = 'Block/Unblock/Deletion Flag'
+        self.ws['C12'] = 'Sold-to'
+        self.ws['E5'] = CN
 
     def set_central_order_block(self):
-        self.ws.Range('E13').Value = 'Set'
-        self.ws.Range('E14').Value = '01 - Overall block'
+        self.ws['E13'] = 'Set'
+        self.ws['E14'] = '01 - Overall block'
         self.zapisywanie()
         self.snow_ch()
         time.sleep(300)
 
     def remove_central_order_block(self):
-        self.ws.Range('E13').Value = 'Reset (Remove)'
-        self.ws.Range('E14').Value = '01 - Overall block'
+        self.ws['E13'] = 'Reset (Remove)'
+        self.ws['E14']= '01 - Overall block'
         self.zapisywanie()
         self.snow_ch()
         time.sleep(300)
 
     def remove_deletion_flag(self):
-        self.ws.Range('E12').Value = 'Reset (Remove)'
+        self.ws['E12']= 'Reset (Remove)'
         self.zapisywanie()
         self.snow_ch("Deletion Flag")
         time.sleep(300)
 
     def remove_central_order_and_deletion_flag(self):
-        self.ws.Range('E12').Value = 'Reset (Remove)'
-        self.ws.Range('E13').Value = 'Reset (Remove)'
-        self.ws.Range('E14').Value = '01 - Overall block'
+        self.ws['E12'] = 'Reset (Remove)'
+        self.ws['E13'] = 'Reset (Remove)'
+        self.ws['E14']= '01 - Overall block'
         self.zapisywanie()
         self.snow_ch("Deletion Flag")
         time.sleep(300)
 
-ch("50517958 ").set_central_order_block()
+de("2").set_central_order_block ()
 
 
 
--- brudnopis.py
+++ brudnopis.py
@@ -228,23 +228,23 @@ from playwright.sync_api import sync_playwright
 # locator("div").nth(5)
 # get_by_text("Log in with")
 
-from playwright.sync_api import sync_playwright
-import time
-
-CN = "0050727431"
-
-with sync_playwright() as p:
-    context = p.chromium.launch_persistent_context(user_data_dir="veeva_profile",headless=False)
-    page = context.new_page()
-    page.goto("https://elanco.veevanetwork.com/ui/",wait_until="networkidle")
-    # page.get_by_role("textbox", name ="User Name").fill("PIOTR.PIENKOWSKI@elancoah.com")
-    # page.get_by_role("textbox", name ="Password").fill("4117Rkxe!!!!")
-    # page.locator("div").nth(5).click()
-    # page.get_by_role("textbox",name= "Enter your email, phone, or").fill("PIOTR.PIENKOWSKI@elancoah.com")
-    # page.get_by_role("button", name = "Next").click()
-    search = page.locator(".input").first
-    search.click()
-    search.fill(CN)
-    search.press("Enter")
-    time.sleep(9999)
+# from playwright.sync_api import sync_playwright
+# import time
+#
+# CN = "0050727431"
+#
+# with sync_playwright() as p:
+#     context = p.chromium.launch_persistent_context(user_data_dir="veeva_profile",headless=False)
+#     page = context.new_page()
+#     page.goto("https://elanco.veevanetwork.com/ui/",wait_until="networkidle")
+#     # page.get_by_role("textbox", name ="User Name").fill("PIOTR.PIENKOWSKI@elancoah.com")
+#     # page.get_by_role("textbox", name ="Password").fill("4117Rkxe!!!!")
+#     # page.locator("div").nth(5).click()
+#     # page.get_by_role("textbox",name= "Enter your email, phone, or").fill("PIOTR.PIENKOWSKI@elancoah.com")
+#     # page.get_by_role("button", name = "Next").click()
+#     search = page.locator(".input").first
+#     search.click()
+#     search.fill(CN)
+#     search.press("Enter")
+#     time.sleep(9999)
 
